6767ce380fcfd0f73da7f87c172d4cdc9f4d5152
Fran Gregori Tandazo <frangregori61@gmail.com>
2023-10-08 13:09:56 +0200

    Realised Worker class

---
+++ php/src/objetos/310-312/Worker.php
@@ -0,0 +1,28 @@
+<?php
+
+abstract class Worker extends Person
+{
+    const RANGE_PAY_TAXES = 35000;
+    private array $phones;
+
+    function __construct( string $nombre, string $apellidos, int $edad )
+    {
+        parent::__construct( $nombre, $apellidos, $edad );
+    }
+
+    public function getPhones(): array
+    {
+        return $this->phones;
+    }
+
+    public function setPhones( array $phones ): void
+    {
+        $this->phones = $phones;
+    }
+
+    abstract function toHTML( Person $person );
+
+    abstract function calculateSalary(): float;
+
+    abstract function havePayTaxes(): bool;
+}
